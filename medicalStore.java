1d907342feee96c6d179aee8e5e42d76b86ca24c
akshay347 <akshaysiddapur562@gmail.com>
2024-07-03 11:12:29 +0530

    basic

---
+++ medicalStore.java
@@ -0,0 +1,10 @@
+class medicalStore{
+
+public static void main(String medi[]){
+System.out.println("Main Started");
+String name="Sri Ventakeshwara Chemist & Drugist";
+String tabletNames[4]={"dolo","haloset","cetragen","okacet"};
+System.out.println("Available Tablets/medicines are"+tabletNames[0]+","+tabletNames[1]+","+tabletNames[2]+","+tabletNames[3]);
+System.out.println("Main Ended");
+}
+}
