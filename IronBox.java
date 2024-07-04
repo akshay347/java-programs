f7b23d106dccaafd60e7e8a77fe5dbb1808a9a13
akshay347 <akshaysiddapur562@gmail.com>
2024-07-04 11:36:22 +0530

    array programs

--- IronBox.java
+++
@@ -1,7 +0,0 @@
-class IronBox{
-public static void main(String[] Bajaj){
- System.out.println("Iron-Box consumes electrcity");
- System.out.println("Iron-Box will help in ironing clotheIrons");
-}
-
-}
